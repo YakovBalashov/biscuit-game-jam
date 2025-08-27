143a175609ffbf38cbb3a21c31fe806d30c0e159
Vladyslav <piskovijvlad1@gmail.com>
2025-08-27 13:35:56 +0200

    implement player jump

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -1,8 +1,51 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerJump : MonoBehaviour
     {
+        [SerializeField] private float fallSpeed;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float groundedRaycastDistance;
+
+        private Rigidbody _rigidbody;
+        private bool _isJumpPressed;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (_rigidbody.linearVelocity.y < 0 || !_isJumpPressed)
+            {
+                _rigidbody.linearVelocity += Vector3.up * (Physics.gravity.y * fallSpeed * Time.fixedDeltaTime);
+            }
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            _isJumpPressed = context.ReadValueAsButton();
+            if (!_isJumpPressed) return;
+            Jump();
+        }
+
+        private void Jump()
+        {
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            if (Math.Abs(_rigidbody.linearVelocity.y) > 0.001f) return false;
+            return Physics.Raycast(transform.position, Vector3.down, out _, groundedRaycastDistance);
+        }
     }
 }
