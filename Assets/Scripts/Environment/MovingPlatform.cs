b957a103fe506129395c3934463f7bdad4602d7f
Yakov Balashov <yakov.balashov@gmail.com>
2025-08-29 21:15:55 +0200

    make player move with moving platform

---
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -0,0 +1,30 @@
+using Player;
+using UnityEngine;
+
+namespace Environment
+{
+    public class MovingPlatform : MonoBehaviour
+    {
+        [SerializeField] private Transform pointA;
+        [SerializeField] private Transform pointB;
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            var pingPong = Mathf.PingPong(Time.time * speed, 1);
+            transform.position = Vector3.Lerp(pointA.position, pointB.position, pingPong);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.GetComponent<PlayerMovement>() == null) return;
+            other.transform.SetParent(transform);
+        }
+        
+        private void OnCollisionExit(Collision other)
+        {
+            if (other.gameObject.GetComponent<PlayerMovement>() == null) return;
+            other.transform.SetParent(null);
+        }
+    }
+}
